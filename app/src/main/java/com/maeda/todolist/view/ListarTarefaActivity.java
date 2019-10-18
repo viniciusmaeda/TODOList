abb8077b91e156a657703e4ec1452d59bef09f80
Vinicius Maeda <viniciusmaeda@gmail.com>
2019-10-17 22:40:28 -0400

    Redirecionamento de tela

--- app/src/main/java/com/maeda/todolist/view/ListarTarefaActivity.java
+++ app/src/main/java/com/maeda/todolist/view/ListarTarefaActivity.java
@@ -1,8 +1,10 @@
 package com.maeda.todolist.view;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.maeda.todolist.R;
 
@@ -12,5 +14,12 @@ public class ListarTarefaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listar_tarefa);
+    }
+
+    public void novaTarefa(View view) {
+        // redireciona pra tela que cadastra nova tarefa
+        startActivity(new Intent(
+                ListarTarefaActivity.this,
+                CadastrarTarefaActivity.class));
     }
 }
