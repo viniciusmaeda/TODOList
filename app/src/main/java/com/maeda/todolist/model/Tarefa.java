18f33ac149000e7ea3821300ce2c670f0ace37c2
Vinicius Maeda <viniciusmaeda@gmail.com>
2019-10-17 21:16:02 -0400

    Initial commit

---
+++ app/src/main/java/com/maeda/todolist/model/Tarefa.java
@@ -0,0 +1,58 @@
+package com.maeda.todolist.model;
+
+public class Tarefa {
+
+    private int id;
+    private String tarefa;
+    private String descricao;
+    private String data;
+    private String hora;
+
+    public Tarefa() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTarefa() {
+        return tarefa;
+    }
+
+    public void setTarefa(String tarefa) {
+        this.tarefa = tarefa;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    @Override
+    public String toString() {
+        return tarefa;
+    }
+}
---
+++ app/src/main/java/com/maeda/todolist/view/ListarTarefaActivity.java
@@ -0,0 +1,16 @@
+package com.maeda.todolist.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.maeda.todolist.R;
+
+public class ListarTarefaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_tarefa);
+    }
+}
